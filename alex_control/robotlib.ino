ccb856e2f970b346aa19ba6e8f60d1fe9cd4a08c
Nickyi29 <tikyikyaw29@gmail.com>
2026-03-21 13:58:25 +0800

    Refactor motor control functions and enums

--- alex_control/robotlib.ino
+++ alex_control/robotlib.ino
@@ -1,90 +1,90 @@
 #include <AFMotor.h>
-// Direction values
-typedef enum dir
-{
-  STOP,
+
+typedef enum {
+  STOP = 0,
   GO,
   BACK,
   CCW,
   CW
 } dir;
 
-// Motor control
-#define FRONT_LEFT   4 // M4 on the driver shield
-#define FRONT_RIGHT  1 // M1 on the driver shield
-#define BACK_LEFT    3 // M3 on the driver shield
-#define BACK_RIGHT   2 // M2 on the driver shield
+// Motor mapping from your current file
+#define FRONT_LEFT   4   // M4
+#define FRONT_RIGHT  1   // M1
+#define BACK_LEFT    3   // M3
+#define BACK_RIGHT   2   // M2
 
 AF_DCMotor motorFL(FRONT_LEFT);
 AF_DCMotor motorFR(FRONT_RIGHT);
 AF_DCMotor motorBL(BACK_LEFT);
 AF_DCMotor motorBR(BACK_RIGHT);
 
-void move(int speed, int direction)
-{
-  
+static void setAllSpeeds(uint8_t speed) {
+  speed = constrain(speed, 0, 255);
   motorFL.setSpeed(speed);
   motorFR.setSpeed(speed);
   motorBL.setSpeed(speed);
   motorBR.setSpeed(speed);
-
-  switch(direction)
-    {
-      case BACK:
-        motorFL.run(BACKWARD);
-        motorFR.run(BACKWARD);
-        motorBL.run(FORWARD);
-        motorBR.run(FORWARD); 
-      break;
-      case GO:
-        motorFL.run(FORWARD);
-        motorFR.run(FORWARD);
-        motorBL.run(BACKWARD);
-        motorBR.run(BACKWARD); 
-      break;
-      case CW:
-        motorFL.run(BACKWARD);
-        motorFR.run(FORWARD);
-        motorBL.run(FORWARD);
-        motorBR.run(BACKWARD); 
-      break;
-      case CCW:
-        motorFL.run(FORWARD);
-        motorFR.run(BACKWARD);
-        motorBL.run(BACKWARD);
-        motorBR.run(FORWARD); 
-      break;
-      case STOP:
-      default:
-        motorFL.run(RELEASE);
-        motorFR.run(RELEASE);
-        motorBL.run(RELEASE);
-        motorBR.run(RELEASE); 
-    }
 }
 
-void forward(int speed)
-{
+void move(uint8_t speed, dir direction) {
+  setAllSpeeds(speed);
+
+  switch (direction) {
+    case GO:
+      motorFL.run(FORWARD);
+      motorFR.run(FORWARD);
+      motorBL.run(BACKWARD);
+      motorBR.run(BACKWARD);
+      break;
+
+    case BACK:
+      motorFL.run(BACKWARD);
+      motorFR.run(BACKWARD);
+      motorBL.run(FORWARD);
+      motorBR.run(FORWARD);
+      break;
+
+    case CW:
+      motorFL.run(BACKWARD);
+      motorFR.run(FORWARD);
+      motorBL.run(FORWARD);
+      motorBR.run(BACKWARD);
+      break;
+
+    case CCW:
+      motorFL.run(FORWARD);
+      motorFR.run(BACKWARD);
+      motorBL.run(BACKWARD);
+      motorBR.run(FORWARD);
+      break;
+
+    case STOP:
+    default:
+      motorFL.run(RELEASE);
+      motorFR.run(RELEASE);
+      motorBL.run(RELEASE);
+      motorBR.run(RELEASE);
+      break;
+  }
+}
+
+void forward(uint8_t speed) {
   move(speed, GO);
 }
 
-void backward(int speed)
-{
+void backward(uint8_t speed) {
   move(speed, BACK);
 }
 
-void ccw(int speed)
-{
+void ccw(uint8_t speed) {
   move(speed, CCW);
 }
 
-void cw(int speed)
-{
+void cw(uint8_t speed) {
   move(speed, CW);
 }
 
-void stop()
-{
+void stop() {
   move(0, STOP);
 }
-
